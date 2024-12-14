c00f876cb40aa4003f0d382e7340896f4047f748
Serkan Ertan <s.ertan@lmu.de>
2024-12-14 16:42:14 +0100

    feature: add ui

--- src/core/layout/index.tsx
+++ src/core/layout/index.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from 'next';
 import { sans } from '@/src/core/font';
 
 import "@/src/core/styles/globals.css";
-import { TheHeader } from '@/src/shared/ui/the-header';
-import { AuthWrapper } from '@/src/shared/ui/auth-wrapper';
+import { TheHeader } from '@/src/shared/components/the-header';
+import { AuthWrapper } from '@/src/shared/components/auth-wrapper';
 
 export const metadata: Metadata = {
-  title: 'Tatilbil | Your AI assitant for perfect holiday!',
+  title: 'Tatilbul | Your AI assitant for perfect holiday!',
   description:
     'Tatilbul is a AI assitant for searching a perfect holiday in internet.',
 };
--- src/screens/home/ui/home-screen.tsx
+++ src/screens/home/ui/home-screen.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/src/shared/ui/button";
 
 
 export async function HomeScreen() {
@@ -8,7 +9,13 @@ export async function HomeScreen() {
     return (
         <>
             <h1>Home Screen</h1>
-
+            <Button>Start</Button>
+            <div className="bg-background text-foreground">
+                Bu div, yeni arka plan rengine sahip!
+            </div>
+            <div className="bg-[hsl(211,95%,54%)] text-[hsl(240,10%,3.9%)]">
+                Bu div, elle tanımlanmış HSL rengini kullanıyor!
+            </div>
         </>
     )
 }
